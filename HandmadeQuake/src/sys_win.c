6d4e5609c2a118695186f103b86be16a02dc65de
KareemErgawy <kareem_ergawy@yahoo.com>
2016-01-16 11:56:35 +0100

    1.3 and 1.4

--- HandmadeQuake/src/sys_win.c
+++ HandmadeQuake/src/sys_win.c
@@ -1,59 +1,12 @@
-#include <windows.h>
+#include "winquake.h"
+#include "quakedef.h"
 
-#define MAX_NUM_ARGVS 50
-
-int argc = 1;
-char* largv[MAX_NUM_ARGVS];
-
-int COM_CheckParm(char* parm)
+int32 CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int32 nCmdShow)
 {
-    for(int i=1 ; i<argc ; i++)
-    {
-        if(!strcmp(parm, largv[i]))
-        {
-            return i;
-        }
-    }
+	COM_ParseCmdLine(lpCmdLine);
 
-    return 0;
-}
-    
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-
-    largv[0] = "";  // this would be the same as largv[argc] = 0;
-    
-    while(*lpCmdLine && (argc < MAX_NUM_ARGVS))
-    {
-        while(*lpCmdLine && ((*lpCmdLine < 32)
-                                 && (*lpCmdLine >= 126)))
-        {
-            lpCmdLine++;
-        }
-        
-        if(*lpCmdLine)
-        {
-            largv[argc] = lpCmdLine;
-            argc++;
-
-            while(*lpCmdLine && ((*lpCmdLine > 32)
-                                 && (*lpCmdLine <= 126)))
-            {
-                lpCmdLine++;
-            }
-
-            if(*lpCmdLine)
-            {
-                *lpCmdLine = 0;
-                lpCmdLine++;
-            }
-        }
-    }
-
-    largv[argc] = "";
-
-    int test = COM_CheckParm("-setalpha");
-    int value = (int)(*largv[3]);
+    int32 test = COM_CheckParm("-setalpha");
+    int32 value = Q_atoi(com_argv[test+1]);
     
     return 0;
 }
